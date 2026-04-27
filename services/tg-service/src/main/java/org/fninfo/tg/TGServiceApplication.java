4c5efbe4ce9131089f8ace6654bda6b170bea08c
nanapap1 <markmoskvitin1007@gmail.com>
2026-04-27 16:50:29 +0300

    Add reddis docker and update enviroment

---
+++ services/tg-service/src/main/java/org/fninfo/tg/TGServiceApplication.java
@@ -0,0 +1,13 @@
+package org.fninfo.tg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TGServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TGServiceApplication.class, args);
+	}
+
+}
